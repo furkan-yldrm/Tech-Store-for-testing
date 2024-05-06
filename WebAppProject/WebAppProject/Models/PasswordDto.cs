40a65e9bb06ba0664d0ac5c8900eed53278036b8
Furkan Yıldırım <yildirim.frkn@outlook.com>
2024-05-06 15:14:14 +0300

    password

---
+++ WebAppProject/WebAppProject/Models/PasswordDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppProject.Models
+{
+	public class PasswordDto
+	{
+		[Required(ErrorMessage = "Mevcut şifreyi girin."), MaxLength(100)]
+		public string CurrentPassword { get; set; } = "";
+
+		[Required(ErrorMessage = "Yeni şifreyi girin"), MaxLength(100)]
+		public string NewPassword { get; set; } = "";
+
+		[Required(ErrorMessage = "Şifreyi onayla")]
+		[Compare("NewPassword", ErrorMessage = "Şifreler eşleşmiyor!!")]
+		public string ConfirmPassword { get; set; } = "";
+	}
+}
