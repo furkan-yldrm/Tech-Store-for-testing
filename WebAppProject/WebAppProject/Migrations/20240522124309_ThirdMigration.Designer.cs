8d3b75c9a0a37949b26fb5c94f61f1b5584e56dd
Furkan Yıldırım <yildirim.frkn@outlook.com>
2024-05-22 15:45:41 +0300

    order entity and table created

--- WebAppProject/WebAppProject/Migrations/20240522124309_ThirdMigration.Designer.cs
+++ WebAppProject/WebAppProject/Migrations/20240522124309_ThirdMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAppProject.Services;
 
@@ -11,9 +12,11 @@ using WebAppProject.Services;
 namespace WebAppProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240522124309_ThirdMigration")]
+    partial class ThirdMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- WebAppProject/WebAppProject/Services/ApplicationDbContext.cs
+++ WebAppProject/WebAppProject/Services/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace WebAppProject.Services
         }
 
         public DbSet<Product> Products { get; set; }
-    }
+		public DbSet<Order> Orders { get; set; }
+	}
 }
